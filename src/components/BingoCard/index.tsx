9c24e2331809efe674bdfe046c0955bb426dd971
Analia Mok <analiam319@gmail.com>
2023-02-25 20:17:28 -0500

    Initial placeholdering

--- src/components/BingoCard/index.tsx
+++ src/components/BingoCard/index.tsx
@@ -1,5 +1,29 @@
 import { h, Fragment } from 'preact'
 
+import style from './style.css'
+
 export const BingoCard = () => {
-  return <Fragment>Hello</Fragment>
+  return (
+    <Fragment>
+      <table class={style.bingoCard}>
+        <tbody>
+          <tr>
+            <td>A</td>
+            <td>B</td>
+            <td>C</td>
+          </tr>
+          <tr>
+            <td>D</td>
+            <td>Free</td>
+            <td>E</td>
+          </tr>
+          <tr>
+            <td>F</td>
+            <td>G</td>
+            <td>H</td>
+          </tr>
+        </tbody>
+      </table>
+    </Fragment>
+  )
 }
--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,9 +1,12 @@
 import { h } from 'preact'
+import { BingoCard } from './BingoCard'
+import 'modern-normalize/modern-normalize.css'
 
 const App = () => (
   <div id='app'>
     <main>
       <h1>Bug Hunting Bingo</h1>
+      <BingoCard />
     </main>
   </div>
 )
